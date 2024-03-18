2ae0cce9a423083a6c5528eee52577b7787a72e3
Koutsura <m.denny.trilisandi@gmail.com>
2024-03-18 08:47:49 +0700

    dokumen + layanan aplikasi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,8 @@
         <p>PSI JAYA JAYA JAYA</p>
         @include('carousel-news')
     </div>
-
+    @include('layanan-aplikasi')
+    @include('dokumen')
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
